1fe1f033c54bca24e3a9d32f157f3efe13d3d250
Sara Strong <sara.n.strong@gmail.com>
2021-09-19 15:33:51 -0400

    useLocalStorage hook

---
+++ src/useLocalStorage.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+function useLocalStorage(key, firstValue = null) {
+  const initialValue = localStorage.getItem(key) || firstValue;
+  let [item, setItem] = useState(initialValue);
+
+  useEffect(() => {
+    console.debug("hooks useLocalStorage useEffect", "item=", item);
+    if (item === null) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, item);
+    }
+  }, [key, item]);
+
+  return [item, setItem];
+}
+
+export default useLocalStorage;
