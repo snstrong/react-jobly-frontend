2d2cdf069bb9217d258ce0f2fdcf2c3789fde804
Sara Strong <sara.n.strong@gmail.com>
2021-09-24 21:41:32 -0400

    Add JS dependencies for Bootstrap navbar collapse

--- src/Nav.js
+++ src/Nav.js
@@ -59,14 +59,14 @@ function Nav({ logout }) {
         className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
+        data-target="#navbarContent"
+        aria-controls="navbarContent"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse" id="navbarContent">
         {currentUser ? loggedInNav() : loggedOutNav()}
       </div>
     </nav>
